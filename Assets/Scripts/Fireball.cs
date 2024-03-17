2f3ca4a9376ff180bcc19698619ef7c5c7b9f4aa
direct777 <sportlake@mail.ru>
2024-03-17 08:21:15 +0200

    Пройдены новые уроки до Здоровья гоблина.

---
+++ Assets/Scripts/Fireball.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public float speed;
+    public float lifeTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DestroyFireball", lifeTime);
+    }
+
+    // Update is called once per frame
+    /*void Update()
+    {
+       //transform.position +=  transform.forward * speed * Time.deltaTime;
+    }*/
+
+    private void FixedUpdate()
+    {
+        MoveFixedUpdate();
+    }
+    private void MoveFixedUpdate()
+    {
+        transform.position += transform.forward * speed * Time.fixedDeltaTime;
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //if (collision.gameObject.tag == "Enemy")
+        {
+            //Destroy(collision.gameObject);
+            DestroyFireball();
+        }
+    }
+
+    private void DestroyFireball()  
+    {
+        Destroy(gameObject);
+    }
+}
